d44cb7aab6ec6ab34b293a547b9d9634993b4a74
Marcelo Ribeiro <marcelosr@lbv.org.br>
2018-01-11 13:03:22 -0300

    Adicionando exemplo de timeout

---
+++ TimeOut/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeOut
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var tokenSource = new CancellationTokenSource();
+            CancellationToken token = tokenSource.Token;
+            int timeOut = 1000; // 1000 ms
+
+            var task = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(970); // Costuma estourar.
+                //Thread.Sleep(950); // Costuma dar tempo.
+            }, token);
+
+            if (!task.Wait(timeOut, token))
+                Console.WriteLine(String.Format("Estourou o timeout"));
+            else
+                Console.WriteLine(String.Format("Deu tempo!"));
+
+            while (true) ;
+        }
+    }
+}
