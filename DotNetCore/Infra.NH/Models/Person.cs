c25b822a74423b22069e9f9afc4c8d74b8537332
Marcelo Ribeiro <marcelosr@lbv.org.br>
2019-06-21 08:53:37 -0300

    Adicionando propriedade na classe Person no exemplo NHibernate

--- DotNetCore/Infra.NH/Models/Person.cs
+++ DotNetCore/Infra.NH/Models/Person.cs
@@ -8,5 +8,6 @@ namespace Infra.NH.Models
         public virtual string Name { get; set; }
         public virtual DateTime CreationDate { get; set; }
         public virtual DateTime UpdatedDate { get; set; }
+        public virtual int IdTeam { get; set; }
     }
 }
