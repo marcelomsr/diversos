f83316d2fc8d85d2d57c9a1d116d52b1574e7734
LBVDC\MarceloSR <marcelosr@lbv.org.br>
2020-04-13 11:56:36 -0300

    Alterando nome dos arquivos e deixando exemplo de chama assíncrona

---
+++ grpc-master/examples/csharp/Helloworld/GreeterClient/Client.cs
@@ -0,0 +1,50 @@
+// Copyright 2015 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Grpc.Core;
+using Helloworld;
+
+namespace GreeterClient
+{
+  class Client
+  {
+    public static void Main(string[] args)
+    {
+      Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+      var client = new Greeter.GreeterClient(channel);
+      String user = "Marcelo";
+
+      // Síncrona
+      var reply = client.SayHello(new HelloRequest { Name = user });
+      Console.WriteLine("Greeting: " + reply.Message);
+
+      // Assíncrona
+      var replyAsync = client.SayHelloAgainAsync(new HelloRequest { Name = user });
+      var awaiter = replyAsync.GetAwaiter();
+      awaiter.OnCompleted(
+        () =>
+        {
+          var metadata = replyAsync.ResponseAsync;
+          Console.WriteLine("Greeting: " + metadata.Result.Message);
+        }
+        );
+
+      channel.ShutdownAsync().Wait();
+      Console.WriteLine("Press any key to exit...");
+      Console.ReadKey();
+    }
+  }
+}
