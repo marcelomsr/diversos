37165613e749ed9139bc101a9a933901d84ddb8b
LBVDC\MarceloSR <marcelosr@lbv.org.br>
2020-04-23 16:07:41 -0300

    Ajustes na formatação das informações e separação de funções

--- SSH.NET-develop/src/VerifyHourOfPabx/Server.cs
+++ SSH.NET-develop/src/VerifyHourOfPabx/Server.cs
@@ -13,13 +13,30 @@ namespace VerifyHourOfPabx
         public DateTime localDate { get; set; }
         public TimeSpan diffDate { get; set; }
         public String message { get; set; }
-        /*
-        Server(string serverName, DateTime serverDate, DateTime localDate, TimeSpan diffDate)
+
+        public static IList<Server> getServers()
         {
-            this.serverName = serverName;
-            this.serverDate = serverDate;
-            this.localDate = localDate;
-            this.diffDate = diffDate;
-        }*/
+            IList<Server> servers = new List<Server>();
+            servers.Add(new Server() { serverName = "Antibes" });
+            servers.Add(new Server() { serverName = "Beder" });
+            servers.Add(new Server() { serverName = "Ebelfort" });
+            servers.Add(new Server() { serverName = "Grenoble" });
+            servers.Add(new Server() { serverName = "Jyllinge" });
+            servers.Add(new Server() { serverName = "Hamamatsu" });
+            servers.Add(new Server() { serverName = "Kobe" });
+            servers.Add(new Server() { serverName = "Lille" });
+            servers.Add(new Server() { serverName = "Niort" });
+            servers.Add(new Server() { serverName = "Nordby" });
+            servers.Add(new Server() { serverName = "Oita" });
+            servers.Add(new Server() { serverName = "Praesto" });
+            servers.Add(new Server() { serverName = "Rouen" });
+            servers.Add(new Server() { serverName = "Soro" });
+            servers.Add(new Server() { serverName = "Stoholm" });
+            servers.Add(new Server() { serverName = "Vamdrup" });
+            servers.Add(new Server() { serverName = "Viborg" });
+            servers.Add(new Server() { serverName = "Vicky" });
+
+            return servers;
+        }
     }
 }
