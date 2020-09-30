c12c0777de73014e79efff0011231256161be708
LBVDC\MarceloSR <marcelosr@lbv.org.br>
2020-09-30 16:02:39 -0300

    Assim funcionou o server e o clinet, e através de um windows service, funcionou o serviço

---
+++ WindowsService/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace WindowsService
+{
+	static class Program
+	{
+		private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			var config = new NLog.Config.LoggingConfiguration();
+
+			string filename = DateTime.Now.ToString().Replace("/", "-").Replace(":", "-");
+
+			// Targets where to log to: File and Console
+			var logfile = new NLog.Targets.FileTarget("logfile") { FileName = filename + ".txt" };
+			var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+			// Rules for mapping loggers to targets            
+			config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+			config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+			// Apply config           
+			NLog.LogManager.Configuration = config;
+
+			Logger.Info("Iniciou");
+
+			ServiceBase[] ServicesToRun;
+			ServicesToRun = new ServiceBase[]
+			{
+				new Service1()
+			};
+			ServiceBase.Run(ServicesToRun);
+		}
+	}
+}
