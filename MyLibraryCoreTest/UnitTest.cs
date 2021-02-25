e2bd5438bcfe6536e8a46a86f02a7b0a1f40ed79
LBVDC\MarceloSR <marcelosr@lbv.org.br>
2021-02-25 14:45:25 -0300

    Ajustes nos testes da lotofacil

--- MyLibraryCoreTest/UnitTest.cs
+++ MyLibraryCoreTest/UnitTest.cs
@@ -36,18 +36,28 @@ namespace MyLibraryCoreTest
 		}
 
 		[Test]
-		public void LotofacilIsCorrect()
+		public void Lotofacil_WithNumbersParameter_OutOfRange()
 		{
-			Assert.AreEqual(15, SimpleFunctions.Lotofacil().Count);
-			Assert.AreEqual(16, SimpleFunctions.Lotofacil(16).Count);
-
 			// Using an anonymous delegate
 			Assert.Throws<ArgumentOutOfRangeException>(
-			delegate { SimpleFunctions.Lotofacil(19); });
+				delegate
+				{
+					SimpleFunctions.Lotofacil(19);
+				});
 
 			// Using a Lambda expression
 			Assert.Throws<ArgumentOutOfRangeException>(
-			  () => { SimpleFunctions.Lotofacil(14); });
+			  () =>
+			  {
+				  SimpleFunctions.Lotofacil(14);
+			  });
+		}
+
+		[Test]
+		public void Lotofacil_IsCorrect()
+		{
+			Assert.AreEqual(15, SimpleFunctions.Lotofacil().Count);
+			Assert.AreEqual(16, SimpleFunctions.Lotofacil(16).Count);
 
 			var sortedNumbers = SimpleFunctions.Lotofacil();
 			var hashSet = new HashSet<int>(sortedNumbers);
